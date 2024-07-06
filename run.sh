6a50ef3e5948e53086c9660c27d15ac092d2e57d
Tobias Hienzsch <post@tobias-hienzsch.de>
2024-07-06 21:25:49 +0200

    Improve T60 calculation

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ root_dir="$(cd "$(dirname "$0")" && pwd)"
 python_dir="$root_dir/src/python"
 engine_exe="$root_dir/build/pffdtd"
 
-sim_dir="$root_dir/data/sim_data/Tobi/cpu_3"
+sim_dir="$root_dir/data/sim_data/Tobi/cpu"
 sim_name="test_script_Tobi_cart_cpu.py"
 model_dir="$root_dir/data/models/Tobi_Office"
 
@@ -24,10 +24,10 @@ python "$sim_name"
 
 # Run sim
 cd "$sim_dir"
-$engine_exe
+OMP_NUM_THREADS=16 $engine_exe
 
 # Post-process
 cd "$python_dir"
-python -m fdtd.process_outputs --data_dir="$sim_dir" --fcut_lowpass 8000.0 --N_order_lowpass=8 --symmetric --fcut_lowcut 10.0 --N_order_lowcut=4 --air_abs_filter="stokes" --save_wav
-python calc_t60.py
-python -m fdtd.process_outputs --data_dir="$sim_dir" --fcut_lowpass 8000.0 --N_order_lowpass=8 --symmetric --fcut_lowcut 10.0 --N_order_lowcut=4 --air_abs_filter="stokes" --plot
+python -m fdtd.process_outputs --data_dir="$sim_dir" --fcut_lowpass 1000.0 --N_order_lowpass=8 --symmetric --fcut_lowcut 10.0 --N_order_lowcut=4 --air_abs_filter="stokes" --save_wav
+python calc_t60.py "$sim_dir"
+python -m fdtd.process_outputs --data_dir="$sim_dir" --fcut_lowpass 1000.0 --N_order_lowpass=8 --symmetric --fcut_lowcut 10.0 --N_order_lowcut=4 --air_abs_filter="stokes" --plot
--- src/python/calc_t60.py
+++ src/python/calc_t60.py
@@ -1,6 +1,6 @@
-import csv
 import glob
 import os
+import sys
 
 import numpy as np
 import scipy.io.wavfile as wavfile
@@ -24,6 +24,14 @@ def octave_filter(sig, fs, center_freq):
     return filtered_signal
 
 
+def third_octave_filter(sig, fs, center):
+    factor = 2 ** (1/6)  # One-third octave factor
+    low = center / factor
+    high = center * factor
+    sos = signal.butter(4, [low, high], btype='band', fs=fs, output='sos')
+    return signal.sosfilt(sos, sig)
+
+
 def energy_decay_curve(ir):
     edc = np.cumsum(ir[::-1]**2)[::-1]
     edc_db = 10 * np.log10(edc / np.max(edc))
@@ -39,36 +47,87 @@ def calculate_t60(edc_db, fs):
     return t60
 
 
+def ebu_3000_t60_threshold_upper(freqs):
+    times = np.zeros_like(freqs)
+    for i, freq in enumerate(freqs):
+        if freq < 63:
+            times[i] = 0.3
+        elif freq <= 200:
+            times[i] = 0.3 - (0.25 * (freq - 63) / (200 - 63))
+        else:
+            times[i] = 0.05
+    return times
+
+
+def ebu_3000_t60_threshold_lower(freqs):
+    times = np.zeros_like(freqs)
+    for i, freq in enumerate(freqs):
+        if freq < 4000:
+            times[i] = -0.05
+        else:
+            times[i] = -0.10
+    return times
+
+
 def main():
-    center_freqs = [15.625, 31.5, 63, 125, 250, 500, 1000, 2000, 4000]
-    directory = "/Users/tobante/Developer/lib/pffdtd/data/sim_data/Tobi/cpu_2"
+    center_freqs = [15.625, 31.5, 63, 125, 250, 500, 1000, 2000]
+    center_freqs = [
+        20, 25, 31.5, 40, 50, 63, 80, 100, 125, 160,
+        200, 250, 315, 400, 500, 630, 800, 1000
+    ]
+    # 1250, 1600, 2000, 2500, 3150, 4000, 5000, 6300, 8000, 10000, 12500, 16000, 20000
+
+    directory = sys.argv[1]
     files = collect_wav_files(directory, "*_out_normalised.wav")
 
     file_times = []
     for file in files:
         fs, ir = wavfile.read(file)
         t60_times = []
-        print("")
+        print(f"{file}")
         for center_freq in center_freqs:
-            filtered_ir = octave_filter(ir, fs, center_freq)
+            filtered_ir = third_octave_filter(ir, fs, center_freq)
             edc_db = energy_decay_curve(filtered_ir)
             t60 = calculate_t60(edc_db, fs)
             t60_times.append(round(t60, 3))
             print(f"T60 at {center_freq} Hz: {t60:.2f} seconds")
 
-        file_times.append(t60_times)
+        file_times.append(np.array(t60_times))
 
-    with open('out.csv', 'w', newline='') as f:
-        writer = csv.writer(f)
-        writer.writerow(center_freqs)
-        writer.writerows(file_times)
-    # # Optionally plot the EDC for one of the bands
-    # plt.plot(edc_db)
-    # plt.title(f"Energy Decay Curve at {center_freqs[5]} Hz")
-    # plt.xlabel("Time (samples)")
-    # plt.ylabel("Decay (dB)")
-    # plt.grid(True)
-    # plt.show()
+    fig = plt.figure()
+
+    # T60
+    ax = fig.add_subplot(2, 1, 1)
+    ax.margins(0, 0.1)
+    ax.semilogx(center_freqs, file_times[0])
+
+    ax.set_title("T60")
+    ax.set_xlabel("Frequency [Hz]")
+    ax.set_ylabel("Decay [s]")
+
+    ax.set_xlim((center_freqs[0], center_freqs[-1]))
+    ax.set_ylim((0.0, np.max(file_times[0])+0.1))
+    ax.grid(which='both', axis='both')
+
+    # Tolerance
+    diff = np.insert(np.diff(file_times[0]), 0, 0.0)
+    diff = np.insert(file_times[0][:-1]-file_times[0][1:], 0, 0.0)
+
+    ax = fig.add_subplot(2, 1, 2)
+    ax.margins(0, 0.1)
+    ax.semilogx(center_freqs, diff)
+    ax.semilogx(center_freqs, ebu_3000_t60_threshold_upper(center_freqs))
+    ax.semilogx(center_freqs, ebu_3000_t60_threshold_lower(center_freqs))
+
+    ax.set_title(f"T60 Tolerance (EBU Tech 3000)")
+    ax.set_xlabel("Frequency [Hz]")
+    ax.set_ylabel("Difference [s]")
+
+    ax.set_xlim((center_freqs[0], center_freqs[-1]))
+    ax.set_ylim((np.min(diff)-0.1, 0.4))
+    ax.grid(which='both', axis='both')
+
+    plt.show()
 
 
 main()
