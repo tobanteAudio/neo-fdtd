44b3396aad41c3740257ae3f10e9c1a0e6a0fc71
Tobias Hienzsch <post@tobias-hienzsch.de>
2024-07-06 23:11:03 +0200

    Improve T60 plots

--- src/python/calc_t60.py
+++ src/python/calc_t60.py
@@ -6,13 +6,13 @@ import numpy as np
 import scipy.io.wavfile as wavfile
 import scipy.signal as signal
 import matplotlib.pyplot as plt
+from matplotlib.ticker import ScalarFormatter
 
 
 def collect_wav_files(directory, pattern="*.wav"):
     search_pattern = os.path.join(directory, pattern)
     wav_files = glob.glob(search_pattern)
     return wav_files
-
 
 
 def third_octave_filter(sig, fs, center):
@@ -97,38 +97,66 @@ def main():
 
         file_times.append(np.array(t60_times))
 
-    fig = plt.figure()
+    plot_styles = {
+        'axes.edgecolor': 'white',
+        'axes.facecolor': 'white',
+        'axes.grid': True,
+        'axes.grid.which': 'both',
+        'axes.spines.left': False,
+        'axes.spines.right': False,
+        'axes.spines.top': False,
+        'axes.spines.bottom': False,
+        'figure.constrained_layout.use': True,
+        'grid.color': '#CCCCCC',
+        'grid.linewidth': '0.8',
+        'xtick.color': '#666666',
+        'xtick.major.bottom': True,
+        'xtick.minor.bottom': False,
+        'ytick.color': '#666666',
+        'ytick.major.left': True,
+        'ytick.minor.left': False,
+    }
+
+    plt.rcParams.update(plot_styles)
+
+    fig, axs = plt.subplots(2, 1)
+    formatter = ScalarFormatter()
+    formatter.set_scientific(False)
 
     # T60
-    ax = fig.add_subplot(2, 1, 1)
-    ax.margins(0, 0.1)
-    ax.semilogx(center_freqs, file_times[0])
+    axs[0].margins(0, 0.1)
+    axs[0].semilogx(center_freqs, file_times[0])
 
-    ax.set_title("T60")
-    ax.set_xlabel("Frequency [Hz]")
-    ax.set_ylabel("Decay [s]")
+    axs[0].set_title("Reverberation Time (T60)")
+    axs[0].set_ylabel("Decay [s]")
+    axs[0].set_xlabel("Frequency [Hz]")
+    axs[0].xaxis.set_major_formatter(formatter)
 
-    ax.set_xlim((center_freqs[0], center_freqs[-1]))
-    ax.set_ylim((0.0, np.max(file_times[0])+0.1))
-    ax.grid(which='both', axis='both')
+    axs[0].set_xlim((center_freqs[0], center_freqs[-1]))
+    axs[0].set_ylim((0.0, np.max(file_times[0])+0.1))
+
+    axs[0].grid(which='minor', color='#DDDDDD', linestyle=':', linewidth=0.5)
+    axs[0].minorticks_on()
 
     # Tolerance
     diff = np.insert(np.diff(file_times[0]), 0, 0.0)
     diff = np.insert(file_times[0][:-1]-file_times[0][1:], 0, 0.0)
 
-    ax = fig.add_subplot(2, 1, 2)
-    ax.margins(0, 0.1)
-    ax.semilogx(center_freqs, diff)
-    ax.semilogx(center_freqs, ebu_3000_t60_threshold_upper(center_freqs))
-    ax.semilogx(center_freqs, ebu_3000_t60_threshold_lower(center_freqs))
+    axs[1].margins(0, 0.1)
+    axs[1].semilogx(center_freqs, diff)
+    axs[1].semilogx(center_freqs, ebu_3000_t60_threshold_upper(center_freqs))
+    axs[1].semilogx(center_freqs, ebu_3000_t60_threshold_lower(center_freqs))
 
-    ax.set_title(f"T60 Tolerance (EBU Tech 3000)")
-    ax.set_xlabel("Frequency [Hz]")
-    ax.set_ylabel("Difference [s]")
+    axs[1].set_title(f"T60 Tolerance (EBU Tech 3000)")
+    axs[1].set_ylabel("Difference [s]")
+    axs[1].set_xlabel("Frequency [Hz]")
+    axs[1].xaxis.set_major_formatter(formatter)
 
-    ax.set_xlim((center_freqs[0], center_freqs[-1]))
-    ax.set_ylim((np.min(diff)-0.1, 0.4))
-    ax.grid(which='both', axis='both')
+    axs[1].set_xlim((center_freqs[0], center_freqs[-1]))
+    axs[1].set_ylim((np.min(diff)-0.1, 0.4))
+
+    axs[1].grid(which='minor', color='#DDDDDD', linestyle=':', linewidth=0.5)
+    axs[1].minorticks_on()
 
     plt.show()
 
