9d2a3217d04f1725749e1a0a7537d0cbada9aa17
Tobias Hienzsch <post@tobias-hienzsch.de>
2024-07-07 06:22:28 +0200

    Add source & recivers to RoomBuilder

--- src/python/common/room_builder.py
+++ src/python/common/room_builder.py
@@ -103,6 +103,20 @@ class RoomBuilder:
         self.colors = colors
         return self
 
+    def add_source(self, name, position):
+        self.sources.append({
+            "name": name,
+            "xyz": position,
+        })
+        return self
+
+    def add_receiver(self, name, position):
+        self.receivers.append({
+            "name": name,
+            "xyz": position,
+        })
+        return self
+
     def add_box(self, material, size, position, rotation=None):
         self.boxes[material].append({
             "size": size,
@@ -207,8 +221,11 @@ def main():
     H = 3.12
 
     builder = RoomBuilder(W, L, H, wall_color=[255, 255, 255])
+    builder.add_source("Speaker Left", [0.2, L-2.52-1.25+0.1, 0.8])
+    builder.add_source("Speaker Right", [0.2, L-2.52-0.2, 0.8])
+    builder.add_receiver("Listener", [1.0, L-2.52-1.4/2, 1.2])
     builder.add_box("Sofa", [0.98, 2.52, 0.48], [0.05, L-2.52-0.05, 0.06])
-    builder.add_box("Table", [0.75, 1.25, 0.02], [0.05, L-2.52-1.4, 0.66])
+    builder.add_box("Table", [0.75, 1.25, 0.02], [0.05, L-2.52-0.05*2-1.25, 0.66])
     builder.add_box("Table", [0.8, 1.8, 0.02], [W-0.8-0.05, 1, 0.7])
     builder.add_box("Table", [0.8, 0.8, 0.02], [W/2-0.4, L-1-0.4, 0.3])
     builder.add_box("TV_Stand", [0.5, 1.6, 0.35], [W-0.5-0.05, L-2, 0.03])
