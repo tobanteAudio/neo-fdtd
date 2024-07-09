d3d247b22ed34c71c907f7f6d8de9550adb3922f
Tobias Hienzsch <post@tobias-hienzsch.de>
2024-07-09 08:14:31 +0200

    Improve diffusor simulation

--- src/2D/fdtd.py
+++ src/2D/fdtd.py
@@ -1,19 +1,22 @@
+import cv2
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 import numpy as np
+from tqdm import tqdm
 
 
 def add_diffusor(width, max_depth, in_mask, X, Y):
     depths = np.array([0.0, 0.25, 1.0, 0.5, 0.5, 1.0, 0.25])
-    depths = np.array([0.0,0.06,0.25,0.56,1.0,0.5,0.12,0.94,0.81,0.81,0.94,0.12,0.5,1,0.56,0.25,0.06])
-    assert depths.shape[0]==17
+    depths = np.array([0.0, 0.06, 0.25, 0.56, 1.0, 0.5, 0.12,
+                      0.94, 0.81, 0.81, 0.94, 0.12, 0.5, 1, 0.56, 0.25, 0.06])
+    assert depths.shape[0] == 17
     prime = depths.shape[0]
-    n = int(6/width)
+    n = int(8/width)
     for w in range(n):
-        xs = 2+w*width
+        xs = 6+w*width
         xe = xs+width
-        ys = 1
-        ye = ys+depths[w % prime] * max_depth
+        ys = 4
+        ye = ys+depths[w % prime] * max_depth+0.03
         in_mask[(X >= xs) & (Y >= ys) & (X < xe) & (Y < ye)] = False
 
     return in_mask
@@ -21,12 +24,12 @@ def add_diffusor(width, max_depth, in_mask, X, Y):
 
 def main():
     c = 343  # speed of sound m/s (20degC)
-    fmax = 4_000  # Hz
-    PPW = 7.7  # points per wavelength at fmax
+    fmax = 1000  # Hz
+    PPW = 10.5  # points per wavelength at fmax
     duration = 0.05  # seconds
-    refl_coeff = 0.9  # reflection coefficient
+    refl_coeff = 0.99  # reflection coefficient
 
-    Bx, By = 10.0, 10.0  # box dims (with lower corner at origin)
+    Bx, By = 20.0, 20.0  # box dims (with lower corner at origin)
     x_in, y_in = Bx*0.5, By*0.5  # source input position
     R_dome = By*0.5  # heigh of dome (to be centered on roof of box)
 
@@ -48,15 +51,11 @@ def main():
     # calculate grid spacing, time step, sample rate
     dx = c/fmax/PPW  # grid spacing
     dt = np.sqrt(0.5)*dx/c
-    SR = 1/dt
+    fs = 1/dt
 
     Nx = int(np.ceil(Lx/dx)+2)  # number of points in x-dir
     Ny = int(np.ceil(Ly/dx)+2)  # number of points in y-dir
     Nt = int(np.ceil(duration/dt))  # number of time-steps to compute
-
-    print(f'SR = {SR:.3f} Hz')
-    print(f'Δx = {dx*100:.5f} cm / {dx*1000:.2f} mm')
-    print(f'Nx = {int(Nx)} Ny = {int(Ny)} Nt = {int(Nt)}')
 
     # x and y sampling points
     xv = np.arange(0, Nx) * dx - 0.5 * dx
@@ -70,7 +69,7 @@ def main():
     if add_dome:
         in_mask[(X - 0.5 * Bx)**2 + (Y - By)**2 < R_dome**2] = True
 
-    in_mask = add_diffusor(dx*4, 0.6, in_mask, X, Y)
+    in_mask = add_diffusor(dx*2, 1.0, in_mask, X, Y)
 
     if apply_rigid:
         # Calculate number of interior neighbours (for interior points only)
@@ -102,7 +101,7 @@ def main():
     u_in = np.zeros(Nt, dtype=np.float64)
     u_in[0] = 1.0
 
-    # Nh = int(np.ceil(5 * SR / fmax))
+    # Nh = int(np.ceil(5 * fs / fmax))
     # n = np.arange(Nh)
     # u_in[:Nh] = 0.5 - 0.5 * np.cos(2 * np.pi * n / Nh)
     # u_in[:Nh] *= np.sin(2 * np.pi * n / Nh)
@@ -111,15 +110,25 @@ def main():
     u1 = np.zeros((Nx, Ny), dtype=np.float64)
     u2 = np.zeros((Nx, Ny), dtype=np.float64)
 
-    print(f"{u2.shape=}")
-    print(f"{in_mask.shape=}")
-    print(f"{ib.shape=}")
-    print(f"{Kib.shape=}")
-    print(f"{Kib[0]}")
+    sps30 = dt*30
+    target_sps = 0.115
+    fps = int(min(200, target_sps/sps30))
 
-    frames = []
+    video_name = 'output_video.avi'
+    height, width = u0.shape
+    fourcc = cv2.VideoWriter_fourcc(*'DIVX')
+    video = cv2.VideoWriter(video_name, fourcc, fps,
+                            (width, height), isColor=False)
 
-    for nt in range(Nt):
+    print(f'fmax = {fmax:.3f} Hz')
+    print(f'fs   = {fs:.3f} Hz')
+    print(f'Δx   = {dx*100:.5f} cm / {dx*1000:.2f} mm')
+    print(f'fps  = {fps}')
+    print(f'Nx   = {int(Nx)}')
+    print(f'Ny   = {int(Ny)}')
+    print(f'Nt   = {int(Nt)}')
+
+    for nt in tqdm(range(Nt)):
         # AIR
         left = u1[0:-2, 1:-1]
         right = u1[2:, 1:-1]
@@ -138,45 +147,50 @@ def main():
             last1 = u1.flat[ib]
             last2 = u2.flat[ib]
 
-            u0.flat[ib] = (2 - 0.5 * Kib) * last1 + 0.5 * (left + right + top + bottom) - last2
+            u0.flat[ib] = (2 - 0.5 * Kib) * last1 + 0.5 * \
+                (left + right + top + bottom) - last2
 
             if apply_loss:
-                u0.flat[ib] = (u0.flat[ib] + lf * (4 - Kib) * u2.flat[ib]) / (1 + lf * (4 - Kib))
+                u0.flat[ib] = (u0.flat[ib] + lf * (4 - Kib) *
+                               u2.flat[ib]) / (1 + lf * (4 - Kib))
 
         u0[inx, iny] = u0[inx, iny] + u_in[nt]
 
         u2 = u1.copy()
         u1 = u0.copy()
 
-        if nt < 2000:
-            frames.append(u0.copy())
+        img = cv2.normalize(u0, None, 0, 255, cv2.NORM_MINMAX).astype(np.uint8)
+        img[~in_mask] = 255
+        video.write(cv2.rotate(img, cv2.ROTATE_90_COUNTERCLOCKWISE))
+
+    video.release()
 
     print(f"last: u0={u0[inx, iny]} u1={u1[inx, iny]} u2={u2[inx, iny]}")
 
-    fig = plt.figure()
+    # fig = plt.figure()
 
-    def draw_func(i):
-        plt.cla()
-        plt.clf()
-        plt.xlim([np.min(xv), np.max(xv)])
-        plt.ylim([np.min(yv), np.max(yv)])
-        img = plt.imshow(
-            (frames[i] * draw_mask).T,
-            extent=(np.min(xv), np.max(xv), np.min(yv), np.max(yv)),
-            cmap="bone",
-            aspect="equal",
-            origin="lower"
-            # vmin=0.0,
-            # vmax=0.05
-        )
-        # Add minorticks on the colorbar to make
-        # it easy to read the values off the colorbar.
-        color_bar = fig.colorbar(img, extend = 'both')
+    # def draw_func(i):
+    #     plt.cla()
+    #     plt.clf()
+    #     plt.xlim([np.min(xv), np.max(xv)])
+    #     plt.ylim([np.min(yv), np.max(yv)])
+    #     img = plt.imshow(
+    #         (frames[i] * draw_mask).T,
+    #         extent=(np.min(xv), np.max(xv), np.min(yv), np.max(yv)),
+    #         cmap="bone",
+    #         aspect="equal",
+    #         origin="lower"
+    #         # vmin=0.0,
+    #         # vmax=0.05
+    #     )
+    #     # Add minorticks on the colorbar to make
+    #     # it easy to read the values off the colorbar.
+    #     color_bar = fig.colorbar(img, extend = 'both')
 
-        color_bar.minorticks_on()
+    #     color_bar.minorticks_on()
 
-    ani = FuncAnimation(fig, draw_func, frames=len(frames), interval=16)
-    plt.show()
+    # ani = FuncAnimation(fig, draw_func, frames=len(frames), interval=16)
+    # plt.show()
 
 
 if __name__ == "__main__":
