cc88e5af4b1ad1ca81d3176e9bbf75ddb64d8ba1
Tobias Hienzsch <post@tobias-hienzsch.de>
2024-07-09 19:57:09 +0200

    Write sycl results to hdf5 files

--- src/2D/fdtd.cpp
+++ src/2D/fdtd.cpp
@@ -5,6 +5,7 @@
 #include <array>
 #include <cassert>
 #include <cstdint>
+#include <span>
 #include <stdexcept>
 #include <string>
 #include <vector>
@@ -13,11 +14,11 @@ template <typename T> inline constexpr auto isStdVector = false;
 
 template <typename T> inline constexpr auto isStdVector<std::vector<T>> = true;
 
-struct H5FFile {
-  explicit H5FFile(char const *str)
+struct H5FReader {
+  explicit H5FReader(char const *str)
       : _handle{H5Fopen(str, H5F_ACC_RDONLY, H5P_DEFAULT)} {}
 
-  ~H5FFile() { H5Fclose(_handle); }
+  ~H5FReader() { H5Fclose(_handle); }
 
   template <typename T> [[nodiscard]] auto read(char const *dataset) -> T {
     if constexpr (isStdVector<T>) {
@@ -99,8 +100,44 @@ private:
   hid_t _handle;
 };
 
+struct H5FWriter {
+  explicit H5FWriter(char const *path)
+      : _handle{H5Fcreate(path, H5F_ACC_TRUNC, H5P_DEFAULT, H5P_DEFAULT)} {}
+
+  ~H5FWriter() { H5Fclose(_handle); }
+
+  auto write(char const *name, std::span<double const> buf, size_t Nx,
+             size_t Ny) -> void {
+    hsize_t dims[2]{Nx, Ny};
+
+    auto def = H5P_DEFAULT;
+    auto type = H5T_NATIVE_DOUBLE;
+
+    auto space = H5Screate_simple(2, dims, NULL);
+    auto set = H5Dcreate(_handle, name, type, space, def, def, def);
+    auto status = H5Dwrite(set, type, H5S_ALL, H5S_ALL, def, buf.data());
+
+    if (status != 0) {
+      throw std::runtime_error("error writing dataset\n");
+    }
+
+    status = H5Dclose(set);
+    if (status != 0) {
+      throw std::runtime_error("error closing dataset\n");
+    }
+
+    status = H5Sclose(space);
+    if (status != 0) {
+      throw std::runtime_error("error closing dataset space\n");
+    }
+  }
+
+private:
+  hid_t _handle;
+};
+
 int main(int, char **argv) {
-  auto file = H5FFile{argv[1]};
+  auto file = H5FReader{argv[1]};
 
   auto const Nx = file.read<int64_t>("Nx");
   auto const Ny = file.read<int64_t>("Ny");
@@ -111,10 +148,11 @@ int main(int, char **argv) {
   auto const adj_bn = file.read<std::vector<int64_t>>("adj_bn");
   auto const bn_ixy = file.read<std::vector<int64_t>>("bn_ixy");
   auto const in_mask = file.read<std::vector<uint8_t>>("in_mask");
-  auto const receiver_ixy = file.read<std::vector<uint8_t>>("receiver_ixy");
+  auto const out_ixy = file.read<std::vector<int64_t>>("out_ixy");
   auto const src_sig = file.read<std::vector<double>>("src_sig");
 
   auto const N = size_t(Nx * Ny);
+  auto const Nr = out_ixy.size();
 
   std::printf("Nt: %ld\n", static_cast<long>(Nt));
   std::printf("Nx: %ld\n", static_cast<long>(Nx));
@@ -125,21 +163,22 @@ int main(int, char **argv) {
   std::printf("in_mask: %ld\n", static_cast<long>(in_mask.size()));
   std::printf("bn_ixy: %ld\n", static_cast<long>(bn_ixy.size()));
   std::printf("adj_bn: %ld\n", static_cast<long>(adj_bn.size()));
-  std::printf("receiver_ixy: %ld\n", static_cast<long>(receiver_ixy.size()));
+  std::printf("out_ixy: %ld\n", static_cast<long>(Nr));
   std::printf("src_sig: %ld\n", static_cast<long>(src_sig.size()));
   std::printf("loss_factor: %f\n", loss_factor);
 
-  auto queueProp = sycl::property_list{sycl::property::queue::in_order()};
-  auto queue = sycl::queue{queueProp};
+  auto prop = sycl::property_list{sycl::property::queue::in_order()};
+  auto queue = sycl::queue{prop};
 
   auto u0 = sycl::buffer<double, 2>(sycl::range<2>(Nx, Ny));
   auto u1 = sycl::buffer<double, 2>(sycl::range<2>(Nx, Ny));
   auto u2 = sycl::buffer<double, 2>(sycl::range<2>(Nx, Ny));
-  auto out = sycl::buffer<double, 2>(sycl::range<2>(receiver_ixy.size(), Nt));
+  auto out = sycl::buffer<double, 2>(sycl::range<2>(Nr, Nt));
 
   auto in_mask_buf = sycl::buffer<uint8_t, 1>{in_mask};
   auto bn_ixy_buf = sycl::buffer<int64_t, 1>{bn_ixy};
   auto adj_bn_buf = sycl::buffer<int64_t, 1>{adj_bn};
+  auto out_ixy_buf = sycl::buffer<int64_t, 1>{out_ixy};
   auto src_sig_buf = sycl::buffer<double, 1>{src_sig};
 
   std::printf("111111111");
@@ -187,12 +226,15 @@ int main(int, char **argv) {
 
     queue.submit([&](sycl::handler &cgh) {
       auto u0_acc = sycl::accessor{u0, cgh};
-      auto out_acc = sycl::accessor{u1, cgh};
-      cgh.parallel_for<struct CopyOutput>(sycl::range<1>(receiver_ixy.size()),
-                                          [=](sycl::id<1> id) {
-                                            auto const r = id[0];
-                                            out_acc[r] = u0_acc[r];
-                                          });
+      auto out_acc = sycl::accessor{out, cgh};
+      auto out_ixy_acc = sycl::accessor{out_ixy_buf, cgh};
+
+      cgh.parallel_for<struct CopyOutput>(
+          sycl::range<1>(Nr), [=](sycl::id<1> id) {
+            auto const r = id[0];
+            auto const r_ixy = out_ixy_acc[r];
+            out_acc[r][i] = u0_acc.get_pointer()[r_ixy];
+          });
     });
 
     queue.submit([&](sycl::handler &cgh) {
@@ -201,18 +243,28 @@ int main(int, char **argv) {
       auto u2_acc = sycl::accessor{u2, cgh};
       cgh.parallel_for<struct Rotate>(sycl::range<2>(Nx, Ny),
                                       [=](sycl::id<2> id) {
-                                        auto const x = id[0];
-                                        auto const y = id[1];
-                                        u2_acc[x][y] = u1_acc[x][y];
-                                        u1_acc[x][y] = u0_acc[x][y];
+                                        u2_acc[id] = u1_acc[id];
+                                        u1_acc[id] = u0_acc[id];
                                       });
     });
 
     queue.wait_and_throw();
   }
 
-  auto host = sycl::host_accessor{u0, sycl::read_only};
-  std::printf("%f\n", double(host[inx][iny]));
+  auto save = std::vector<double>(Nt * Nr);
+  auto host = sycl::host_accessor{out, sycl::read_only};
+  auto absMax = 0.0;
+  for (auto i{0UL}; i < Nt; ++i) {
+    for (auto r{0UL}; r < Nr; ++r) {
+      save[i * Nr + r] = host[r][i];
+      absMax = std::max(absMax, std::abs(host[r][i]));
+    }
+  }
+
+  std::printf("MAX: %f\n", absMax);
+
+  auto results = H5FWriter{"out.h5"};
+  results.write("out", std::span{save}, Nr, Nt);
 
   return EXIT_SUCCESS;
 }
