0eee98ccfd59b9a3754a2376fa03a8898bb7aea9
Tobias Hienzsch <post@tobias-hienzsch.de>
2024-07-11 00:17:13 +0200

    Add launch script for 2D simulation

---
+++ run_2d.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+set -e
+
+build_dir=cmake-build-sycl
+root_dir="$(cd "$(dirname "$0")" && pwd)"
+python_dir="$root_dir/src/python"
+engine_exe="$root_dir/$build_dir/src/cpp/main_2d/pffdtd_2d"
+
+sim_name="Diffusor"
+sim_dir="$root_dir/data/sim_data/$sim_name/cpu"
+
+fmax=1000
+duration=0.06
+
+# Delete old sim
+rm -rf "$sim_dir"
+
+# Generate model
+cd "$python_dir"
+python -m fdtd2.fdtd --save --video --data_dir="$sim_dir" --duration="$duration" --fmax="$fmax"
+
+# Run sim
+DPCPP_CPU_PLACES=cores DPCPP_CPU_CU_AFFINITY=spread DPCPP_CPU_NUM_CUS=16 "$engine_exe" "$sim_dir/diffusor.h5"
