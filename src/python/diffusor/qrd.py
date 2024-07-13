a79596d37df5cde03c8f4c6ce889aec387196f11
Tobias Hienzsch <post@tobias-hienzsch.de>
2024-07-13 23:54:49 +0200

    [python] Improve QRD calculations with formulas from QRDude

--- src/python/diffusor/qrd.py
+++ src/python/diffusor/qrd.py
@@ -1,15 +1,43 @@
 import numpy as np
 
 
-def quadratic_residue_diffuser(prime, depth):
+def quadratic_residue_diffuser(prime, depth=None):
     n = np.mod(np.arange(0, prime, 1)**2, prime)
-    n = n / np.max(n)
-    return n*depth
+    if depth:
+        n = n / np.max(n)
+        return n*depth
+    return n
 
 
 def main():
-    print(quadratic_residue_diffuser(7, 1.0))
-    print(quadratic_residue_diffuser(17, 1.0))
+    n = 13
+    c = 343
+    well_width = 0.0254*2
+    design_frequency = 400
+
+    design_wavelength = c/design_frequency
+    design_depth = design_wavelength/2
+    plate_frequency = design_frequency*n
+    fmin = design_frequency/2
+    fmax = c/(well_width*2)
+    seat_distance = design_wavelength*3
+
+    w = quadratic_residue_diffuser(n, design_depth)
+
+    print(f"prime     = {n}")
+    print(f"width     = {well_width*100:.2f} cm")
+    print(f"depth     = {design_depth*100:.2f} cm")
+    print(f"seat      = {seat_distance*100:.2f} cm")
+    print("")
+
+    print(f"scatter   = {fmin:.2f} Hz")
+    print(f"diffuse   = {design_frequency:.2f} Hz")
+    print(f"HF cutoff = {fmax:.2f} Hz")
+    print(f"plate     = {plate_frequency:.2f} Hz")
+    print("")
+
+    print(f"wells     = {np.round(w*100,2)} cm")
+    print(f"max depth = {np.max(w)*100:.2f} cm")
 
 
 if __name__ == "__main__":
