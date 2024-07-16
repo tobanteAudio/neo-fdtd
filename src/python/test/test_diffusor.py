9628f3be62d2efc157a401d5206806d4dd62436f
Tobias Hienzsch <post@tobias-hienzsch.de>
2024-07-16 07:39:09 +0200

    Add pytest

---
+++ src/python/test/test_diffusor.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+from ..diffusor.qrd import quadratic_residue_diffuser
+
+
+def test_quadratic_residue_diffuser():
+    w = quadratic_residue_diffuser(5, depth=None)
+    assert np.allclose(w, [0, 1, 4, 4, 1])
+
+    w = quadratic_residue_diffuser(7, depth=None)
+    assert np.allclose(w, [0, 1, 4, 2, 2, 4, 1])
+
+    w = quadratic_residue_diffuser(11, depth=None)
+    assert np.allclose(w, [0, 1, 4, 9, 5, 3, 3, 5, 9, 4, 1])
+
+    w = quadratic_residue_diffuser(5, depth=10)
+    assert np.allclose(w, [0, 2.5, 10, 10, 2.5])
