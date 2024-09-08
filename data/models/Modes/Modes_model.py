bcd67275f8deaad23085be0d0d934951647339d2
Tobias Hienzsch <post@tobias-hienzsch.de>
2024-09-08 14:26:46 +0200

    Improve Modes model

--- data/models/Modes/Modes_model.py
+++ data/models/Modes/Modes_model.py
@@ -1,11 +1,24 @@
-import numpy as np
-
 from pffdtd.sim3d.room_builder import RoomBuilder
+from pffdtd.sim3d.constants import SimConstants
 
 S = 0.4
 L = 7.00*S
 W = 5.19*S
 H = 3.70*S
+
+W = 2.0
+L = 3.0
+H = 4.0
+
+Tc = 20
+rh = 50
+fcc = False
+fmax = 800
+ppw = 10.5
+constants = SimConstants(Tc=Tc, rh=rh, fmax=fmax,
+                         PPW=ppw, fcc=fcc, verbose=False)
+mul = 3.5 if fcc else 2.0
+offset = constants.h * mul
 
 room = RoomBuilder(L, W, H)
 room.with_colors({
@@ -14,10 +27,6 @@ room.with_colors({
     "Walls": [255, 255, 255],
 })
 
-fmax = 800
-ppw = 10.5
-dx = 343/(fmax*ppw)
-offset = dx*2
 
 room.add_source("S1", [offset, offset, offset])
 room.add_receiver("R1", [W-offset, L-offset, H-offset])
