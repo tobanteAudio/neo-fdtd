8b9b59dceeb85bba25e1df9f1534fac9156e75fc
Tobias Hienzsch <post@tobias-hienzsch.de>
2024-09-11 03:58:39 +0200

    Add door and window to LivingRoom model

--- data/models/LivingRoom/LivingRoom_model.py
+++ data/models/LivingRoom/LivingRoom_model.py
@@ -1,6 +1,5 @@
 import pathlib
 
-from pffdtd.sim3d.constants import SimConstants
 from pffdtd.sim3d.model_builder import MeshModelBuilder
 
 
@@ -8,17 +7,19 @@ def main():
     dir = pathlib.Path(".")
     obj = dir/"obj"
 
-    fcc = False
-    constants = SimConstants(Tc=20, rh=50, fmax=2500, PPW=10.5, fcc=fcc)
-    mul = 4.0 if fcc else 2.0
-    offset = constants.h * mul
+    s1 = [3.65-0.4, 6.0-0.3, 1.2]
+    s2 = [3.65-0.4, 6.0-2.3, 1.2]
+
+    r1 = [0.6, 6.0-1.3, 1.1]
+    r2 = [0.6, 6.0-2.0, 1.1]
 
     m = MeshModelBuilder()
-    m.add("Book Shelf", obj / 'book_shelf.obj', [90, 90, 90], reverse=True)
+    m.add("Book Shelf", obj / 'book_shelf.obj', [200, 200, 200], reverse=True)
     m.add("Ceiling", obj / 'ceiling.obj', [150, 150, 150], reverse=True)
     m.add("Coffee Table", obj / 'coffee_table.obj', [10, 10, 10], reverse=True)
     m.add("Couch", obj / 'couch.obj', [29, 50, 112], reverse=True)
     m.add("Desk", obj / 'desk.obj', [103, 70, 55], reverse=True)
+    m.add("Door", obj / 'door.obj', [103, 70, 55], reverse=True)
     m.add("Floor", obj / 'floor.obj', [133, 94, 66], reverse=True)
     m.add("Kallax", obj / 'kallax.obj', [200, 200, 200], reverse=True)
     m.add("Table", obj / 'table.obj', [200, 200, 200], reverse=True)
@@ -26,9 +27,11 @@ def main():
     m.add("TV 55", obj / 'tv_55.obj', [10, 10, 10], reverse=True)
     m.add("TV Table", obj / 'tv_table.obj', [120, 120, 120], reverse=True)
     m.add("Walls", obj / 'walls.obj', [175, 175, 175], reverse=True)
-    m.add_source("S1", [offset, offset, 3.12-offset])
-    m.add_receiver("R1", [3.65-offset, 6-offset, offset])
-    m.add_receiver("R2", [3.65-offset, 6-offset, offset+1.0])
+    m.add("Window", obj / 'window.obj', [137, 207, 240], reverse=True)
+    m.add_source("S1", s1)
+    m.add_source("S2", s2)
+    m.add_receiver("R1", r1)
+    m.add_receiver("R2", r2)
     m.write(dir / "model.json")
 
 
