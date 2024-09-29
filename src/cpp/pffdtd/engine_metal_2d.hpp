a4001afdb9508854e2d8f51f12f2a57e8517c38b
Tobias Hienzsch <post@tobias-hienzsch.de>
2024-09-29 21:57:20 +0200

    [cpp] Fix macOS EngineMETAL2D build

--- src/cpp/pffdtd/engine_metal_2d.hpp
+++ src/cpp/pffdtd/engine_metal_2d.hpp
@@ -14,7 +14,9 @@
 namespace pffdtd {
 
 struct EngineMETAL2D {
-  [[nodiscard]] auto operator()(Simulation2D const& sim) const -> stdex::mdarray<double, stdex::dextents<size_t, 2>>;
+
+  [[nodiscard]] auto operator()(Simulation2D const& sim, Precision precision) const
+      -> stdex::mdarray<double, stdex::dextents<size_t, 2>>;
 };
 
 } // namespace pffdtd
